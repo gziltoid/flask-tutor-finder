5d981e6d73fe5c11d7531500c9fc97e34bd7d28d
gziltoid <g.ziltoid@gmail.com>
2021-04-26 01:26:59 +0200

    Fix heroku issue not connecting to postgres

--- app.py
+++ app.py
@@ -22,10 +22,15 @@ load_dotenv(find_dotenv())
 app = Flask(__name__)
 manager = Manager(app)
 csrf = CSRFProtect(app)
+
+DB_URI = os.getenv("DATABASE_URL")
+if DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
+
 app.config.update(
     SQLALCHEMY_ECHO=False,
     SECRET_KEY=os.getenv("SECRET_KEY"),
-    SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL"),
+    SQLALCHEMY_DATABASE_URI=DB_URI,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
 app.debug = False
